5e0d6e2fb6dc6dfacf4338045536b42b3fba83c6
anantharaman445 <anantharaman45@gmail.com>
2021-07-18 20:45:37 +0530

    lables changed

--- email_ops/get_mail_utils.py
+++ email_ops/get_mail_utils.py
@@ -49,19 +49,33 @@ def insert_records(mail_id, subject, from_add, epoch):
     db_connector_factory.insert_email_records(mail_id, subject, from_add, epoch)
 
 
-def mark_email_unread(email_service, email_message_id):
+def get_message_by_id(email_service, email_message_id):
+    msg = (
+        email_service.users().messages().get(userId="me", id=email_message_id).execute()
+    )
+    return msg
+
+
+def remove_labels(email_service, email_message_id, label_value = ["UNREAD"]):
     modified_msg = (
         email_service.users()
         .messages()
-        .modify(userId="me", id=email_message_id, body={"removeLabelIds": ["UNREAD"]})
+        .modify(userId="me", id=email_message_id, body={"removeLabelIds": label_value})
+        .execute()
+    )
+
+    return modified_msg
+
+def add_labels(email_service, email_message_id, label_value = ["UNREAD"]):
+    modified_msg = (
+        email_service.users()
+        .messages()
+        .modify(userId="me", id=email_message_id, body={"addLabelIds": label_value})
         .execute()
     )
 
     return modified_msg
 
 
-def get_message_by_id(email_service, email_message_id):
-    msg = (
-        email_service.users().messages().get(userId="me", id=email_message_id).execute()
-    )
-    return msg
+
+
