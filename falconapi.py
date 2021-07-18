3eb118229280799279c83277279b201f5af7d1cb
anantharaman445 <anantharaman45@gmail.com>
2021-07-18 21:38:18 +0530

    changes

--- falconapi.py
+++ falconapi.py
@@ -4,9 +4,7 @@ class FilterEmails(object):
     def on_put(self, req, resp):
         pass
 
-#  https://stoplight.io/blog/python-rest-api/
 
-#  https://stackoverflow.com/questions/35778772/how-to-mark-messages-as-read-using-gmail-api-as-i-parse
 
 api = falcon.API()
 filter_endpoint = FilterEmails()
