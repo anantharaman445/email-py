13892db030e5e9ed573f5006624770ce6d1e5d39
anantharaman445 <anantharaman45@gmail.com>
2021-07-19 10:48:32 +0530

    x

--- get_email.py
+++ get_email.py
@@ -2,5 +2,5 @@
 from email_ops.get_email_ops import get_emails
 user_app_token = "./user_token/token.json"
 
-# maxResults is the number of emails the user wanted to fetch at a time. out customised default value is 1
+# maxResults is the number of emails the user wanted to fetch at a time. out customised default value is 2
 get_emails(user_app_token, maxResults=2)
